27b55044e38575418683bbbf8fcc915364a21129
Nathan Bowser <nathan.bowser@spiderstrategies.com>
2012-08-28 12:12:52 -0400

    Adds basic escher functionality for pushing and popping views.
    The example page shows a basic example with three layers of views.

--- src/escher.js
+++ src/escher.js
@@ -5,6 +5,31 @@
  */
 (function(window) {
 
-  "use strict";
+  "use strict"
+
+  var Escher = function (opts) {
+    if (!opts.base) {
+      throw new Error('Escher must receive the base view')
+    }
+    this.base = opts.base
+    // TODO: receive config options
+    this.views = []
+  }
+
+  Escher.prototype.push = function (view) {
+    this.base.$el.append(view.render().el)
+    this.views.push(view)
+  }
+
+  Escher.prototype.pop = function () {
+    var view = this.views.pop()
+    view.remove()
+  }
+
+  Escher.prototype.length = function () {
+    return this.views.length
+  }
+
+  Backbone.Escher = Escher
 
 })(this);
