95bdc55fba9ec80a400c0dd96dad2d8106dfb2e6
Nathan Bowser <nathan.bowser@spiderstrategies.com>
2012-08-30 16:02:49 -0400

    Upgrades the version

--- dist/escher-0.0.2.js
+++ dist/escher-0.0.2.js
@@ -1,5 +1,5 @@
 /*!
- * escher.js v0.0.1 
+ * escher.js v0.0.2 
  * Copyright 2012, Spider Strategies <nathan.bowser@spiderstrategies.com> 
  * escher.js may be freely distributed under the BSD license. 
 */
@@ -27,8 +27,14 @@
     this._step(opts.base)
   }
 
+  Escher.prototype.on = Backbone.Events.on
+  Escher.prototype.off = Backbone.Events.off
+  Escher.prototype.trigger = Backbone.Events.trigger
+
   Escher.prototype._step = function (view) {
     var step = new Step(view, this.opts)
+    view.$el.addClass('escher-step')
+    view.trigger('view:activate')
     step.index = this.steps.push(step) - 1
     step.retreat.on('close', function () {
       var self = this
@@ -47,6 +53,7 @@
   }
 
   Escher.prototype.push = function (view, rendered) {
+    this.trigger('changing')
     // Drop the current step back
     var last = _.last(this.steps).drop()
 
@@ -63,13 +70,16 @@
     // Place it appropriately
     last.view.$el.after(view.el)
     this._step(view)
+    this.trigger('changed')
   }
 
   Escher.prototype.pop = function () {
+    this.trigger('changing')
     if (this.steps.length > 1) {
       this.steps.pop().destroy()
       _.last(this.steps).rise()
     }
+    this.trigger('changed')
   }
 
   Escher.prototype.length = function () {
@@ -88,12 +98,15 @@
     // Add the retreat link
     this.retreat.$el.show()
     this.view.$el.append(this.retreat.$el)
+    this.view.trigger('view:deactivate')
     return this
   }
 
   Step.prototype.destroy = function () {
     this.view.undelegateEvents()
+    this.view.$el.removeClass('escher-step')
     this.view.remove()
+    this.view.trigger('view:deactivate')
     this.retreat.off('close')
     this.retreat.remove()
     this.retreat = null
@@ -102,6 +115,7 @@
   Step.prototype.rise = function () {
     this.view.delegateEvents()
     this.retreat.$el.hide()
+    this.view.trigger('view:activate')
   }
 
   var StepRetreat = Backbone.View.extend({
