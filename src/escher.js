74f8d86526a227abc2ef28e9d82696ad83ccb5e6
Scott O'Reilly <scott.oreilly@spiderstrategies.com>
2012-09-13 22:29:38 -0500

    Added CSS3 Transitions for Webkit Browsers
    I added animated transitions using CSS3.  Right now it only works for webkit browsers, but other browsers just have the standard behavior.

--- src/escher.js
+++ src/escher.js
@@ -98,7 +98,7 @@
     className: 'escher-step',
 
     attributes: {
-      style: 'position: absolute; left: 0; top: 0; width: 100%'
+      style: 'position: absolute; left: -1px; top: -1px; width: 100%'
     },
 
     initialize: function (opts) {
